4d0afadcc89ee55a6a336b7ebe3d872d569ddc94
E54066133 <45085286+E54066133@users.noreply.github.com>
2021-12-10 23:45:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,49 +8,44 @@ import cv2
 from lite_lib import load_labels, set_input_tensor, classify_image
 import RPi.GPIO as GPIO
 
-paper = 8 #設定燈泡腳位，其中紅色(8)是布，綠色(5)是石頭，黃色(3)是剪刀
+paper = 8                      #設定燈泡腳位，其中紅色(8)是布，綠色(5)是石頭，黃色(3)是剪刀
 rock = 5
 sciss0rs = 3
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(paper, GPIO.OUT)
-GPIO.setup(rock, GPIO.OUT)
-GPIO.setup(sciss0rs, GPIO.OUT)
+GPIO.setup(paper, GPIO.OUT)       #設為output腳位
+GPIO.setup(rock, GPIO.OUT)        #設為output腳位
+GPIO.setup(sciss0rs, GPIO.OUT)    #設為output腳位
 
 try:
     GPIO.output(paper, GPIO.LOW)
     GPIO.output(rock, GPIO.LOW)
     GPIO.output(sciss0rs, GPIO.LOW)
     while True:
-        parser = argparse.ArgumentParser(
+        parser = argparse.ArgumentParser(                                   #建立parser
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-        parser.add_argument(
+        parser.add_argument(                                                #parser建置參數model
             '--model', help='File path of .tflite file.', required=True)
-        parser.add_argument(
+        parser.add_argument(                                                #parser建置參數labels
             '--labels', help='File path of labels file.', required=True)
         args = parser.parse_args()
 
-        labels = load_labels(args.labels)
-        camera = Camera()
-        interpreter = Interpreter(args.model)
+        labels = load_labels(args.labels)                                   #載入labels
+        camera = Camera()                                                   #設定 raspberrypi 相機
+        interpreter = Interpreter(args.model)                               #對 model 建立 interpreter
         interpreter.allocate_tensors()
-        #得到model所需要的input大小，將輸入image resize成該大小
-        _, height, width, _ = interpreter.get_input_details()[0]['shape']
+        _, height, width, _ = interpreter.get_input_details()[0]['shape']   #得到model所需要的input大小，將輸入image resize成該大小
         while(True):
-            #get image from camera
-            img = camera.get_frame()
-            image = cv2.resize(img,(width, height))
-
-            #inference image
-            results = classify_image(interpreter, image)
-            label_id, prob = results[0]
+            img = camera.get_frame()                                        #相機拍照(frame)
+            image = cv2.resize(img,(width, height))                         #resize相片，使其符合tslite要求的固定大小
             
-
-            #put inference result on image
+            results = classify_image(interpreter, image)                    #辨識frame中的手勢
+            label_id, prob = results[0]            
+           
             label_text = labels[label_id].split(" ")[1]
             prob_text = 'Prob:' + str(int(prob*100)) + '%'
-            cv2.putText(image, label_text, (90, 40), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
-            cv2.putText(image, prob_text, (80, 60), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
+            cv2.putText(image, label_text, (90, 40), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)   #將辨識的結果輸出在照片上
+            cv2.putText(image, prob_text, (80, 60), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)    #將辨識的可信度輸出在照片上
 
             if label_id == 0:
                 GPIO.output(paper, GPIO.HIGH)
